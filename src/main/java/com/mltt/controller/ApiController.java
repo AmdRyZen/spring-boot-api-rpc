3d0e7cb62a52c3f46544704f9abf7840dfdaa5dc
Tianshizhiwang·Yan <tianshizhiwangyan@gmail.com>
2022-05-31 16:53:04 +0800

    update Triple && Stream

--- src/main/java/com/mltt/controller/ApiController.java
+++ src/main/java/com/mltt/controller/ApiController.java
@@ -4,6 +4,7 @@ import com.alibaba.nacos.api.config.annotation.NacosValue;
 import com.mltt.biz.model.FUser;
 import com.mltt.service.ApiService;
 import com.mltt.service.DubboApiService;
+import org.apache.dubbo.common.stream.StreamObserver;
 import org.apache.dubbo.config.annotation.DubboReference;
 
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -35,5 +36,47 @@ public class ApiController {
         return dubboRpcService.getFuserList();
     }
 
+    @RequestMapping("/serverStream")
+    public Long serverStream() {
+        dubboRpcService.sayHelloServerStream("server stream", new StreamObserver<String>() {
+            @Override
+            public void onNext(String data) {
+                System.out.println(data);
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                throwable.printStackTrace();
+            }
+
+            @Override
+            public void onCompleted() {
+                System.out.println("onCompleted");
+            }
+        });
+
+
+        StreamObserver<String> request = dubboRpcService.sayHelloStream(new StreamObserver<String>() {
+            @Override
+            public void onNext(String data) {
+                System.out.println(data);
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                throwable.printStackTrace();
+            }
+
+            @Override
+            public void onCompleted() {
+                System.out.println("onCompleted");
+            }
+        });
+        for (int i = 0; i < 10; i++) {
+            request.onNext("stream request" + i);
+        }
+        request.onCompleted();
+        return 1L;
+    }
 
 }
--- src/main/java/com/mltt/service/DubboApiService.java
+++ src/main/java/com/mltt/service/DubboApiService.java
@@ -1,9 +1,14 @@
 package com.mltt.service;
 
 import com.mltt.biz.model.FUser;
+import org.apache.dubbo.common.stream.StreamObserver;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface DubboApiService {
     FUser getFuserList();
+
+    void sayHelloServerStream(String server_stream, StreamObserver<String> onCompleted);
+
+    StreamObserver<String> sayHelloStream(StreamObserver<String> onCompleted);
 }
